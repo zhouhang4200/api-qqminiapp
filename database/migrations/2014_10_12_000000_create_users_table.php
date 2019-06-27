3dfcbf32a83fb5390593fa62cf53d1154be9f979
zhouhang <zhouhang4200@163.com>
2019-06-27 12:28:53 +0800

    新增用户表，视频表，分类表等

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->string('openid')->comment('用户唯一标识');
+            $table->string('unionid')->comment('用户在开放平台的唯一标识符');
+            $table->string('name')->nullable();
+            $table->string('phone')->unique()->nullable()->comment('手机号');
+            $table->string('password')->nullable();
+            $table->string('qq')->nullable()->comment('用户qq');
+            $table->string('nickname')->nullable()->comment('小程序昵称');
+            $table->string('email')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2019_06_27_034006_create_videos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVideosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('videos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('category_id');
+            $table->string('title')->comment('标题');
+            $table->string('thumb')->comment('视频logo照');
+            $table->string('url')->comment('视频地址');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('videos');
+    }
+}
