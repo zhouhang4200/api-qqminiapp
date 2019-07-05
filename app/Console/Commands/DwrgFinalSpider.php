759228ed8d969fc541c1fa5753ca76cf21190a8b
zhouhang <zhouhang4200@163.com>
2019-07-05 22:08:31 +0800

    修改爬虫触发事件

--- app/Console/Commands/DwrgFinalSpider.php
+++ app/Console/Commands/DwrgFinalSpider.php
@@ -40,60 +40,66 @@ class DwrgFinalSpider extends Command
      */
     public function handle()
     {
-        $url = '';
+        try {
+            $url = '';
 
-        $puppeteer = new Puppeteer([
-            'debug'        => true,
-            'stop_timeout' => 10,
-            'read_timeout' => 30,
-            'idle_timeout' => 60,
-            'timeout' => 60
-        ]);
+            $puppeteer = new Puppeteer([
+                'debug'        => true,
+                'stop_timeout' => 10,
+                'read_timeout' => 30,
+                'idle_timeout' => 60,
+                'timeout' => 60
+            ]);
 
-        $browser = $puppeteer->launch([
-            'args' => ['--no-sandbox', '--disable-setuid-sandbox'],
-        ]);
+            $browser = $puppeteer->launch([
+                'args' => ['--no-sandbox', '--disable-setuid-sandbox'],
+            ]);
 
-        // 获取数据库里面状态为0的所有的原始地址
-        Video::where('status', 0)
-            ->where('category_id', config('spider.category.dwrg')) // 改第一处
-            ->chunk(100, function ($chunks) use ($url, $browser) {
-                foreach ($chunks as $video) {
-                    try {
-                        $page = $browser->newPage();
+            // 获取数据库里面状态为0的所有的原始地址
+            Video::where('status', 0)
+                ->where('category_id', config('spider.category.dwrg')) // 改第一处
+                ->chunk(100, function ($chunks) use ($url, $browser) {
+                    foreach ($chunks as $video) {
+                        try {
+                            $page = $browser->newPage();
 
-                        $page->goto($video->original_url);
-                        $html = $page->content(); // Prints the HTML
+                            $page->goto($video->original_url);
+                            $html = $page->content(); // Prints the HTML
 
-                        $document = new Document();
-                        $doc      = $document->load($html);
-                        $urlDoc   = $doc->first('video');
+                            $document = new Document();
+                            $doc      = $document->load($html);
+                            $urlDoc   = $doc->first('video');
 
-                        if ($urlDoc) {
-                            $url = $urlDoc->getAttribute('src');
-                        } else {
-                            myLog('dwrg_final_spider_error', ['src不存在！']); // 改第二处
+                            if ($urlDoc) {
+                                $url = $urlDoc->getAttribute('src');
+                            } else {
+                                myLog('dwrg_final_spider_error', ['src不存在！']); // 改第二处
+                                $video->delete();
+                                continue;
+                            }
+
+                            // 写入数据库
+                            if ($url) {
+                                $video->status = 1;
+                                $video->url    = $url;
+                                $video->save();
+                            } else {
+                                $video->delete();
+                                continue;
+                            }
+                            sleep(3);
+                        } catch (\Exception $e) {
                             $video->delete();
+                            myLog('dwrg_final_spider_error', ["【" . $e->getLine() . "】" . $e->getMessage()]); // 改第二处
                             continue;
                         }
-
-                        // 写入数据库
-                        if ($url) {
-                            $video->status = 1;
-                            $video->url    = $url;
-                            $video->save();
-                        } else {
-                            $video->delete();
-                            continue;
-                        }
-                        sleep(3);
-                    } catch (\Exception $e) {
-                        $video->delete();
-                        myLog('dwrg_final_spider_error', ["【" . $e->getLine() . "】" . $e->getMessage()]); // 改第二处
-                        continue;
                     }
-                }
-            });
-        $browser->close();
+                });
+            $browser->close();
+        } catch (\Exception $e) {
+            myLog('dwrg_final_spider_error', ["【" . $e->getLine() . "】" . $e->getMessage()]); // 改第二处
+
+        }
+
     }
 }
