277ecd82d3fd352fbb77de02bc6b2622e851f02b
zhouhang <zhouhang4200@163.com>
2019-07-10 11:26:26 +0800

    增加腾讯爬虫

---
+++ app/Console/Commands/QQVideoSpider.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Category;
+use App\Models\Video;
+use Carbon\Carbon;
+use GuzzleHttp\Client;
+use Illuminate\Console\Command;
+
+class QQVideoSpider extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'qq:video';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '腾讯视频爬虫';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function handle()
+    {
+        $date      = Carbon::now()->toDateString();
+        // 获取所有的除了游戏的分类
+        $categories = Category::where('id', '!=', 1)->get();
+
+        foreach ($categories as $category) {
+            $timestamp = Carbon::now()->subHours(1)->timestamp;
+            for ($i = 1; $i < 20; $i++) {
+                $page = 15 * $i;
+                try {
+                    // 第一步：获取视频id
+                    $searchUrl = "https://node.video.qq.com/x/api/msearch?contextValue=last_end%3D" . $page . "%26sort%3D1%26%26response%3D1&filterValue=pubfilter%3D4&searchSession=qid%3DpK7qYBiMDvhLjL1kGhWIPdbyVshRXm_pptL9YqboWWlIososhSJj1A&keyWord=" . $category->name . "&contextType=3&_=1562670251724&callback=jsonp3";
+                    $client    = new Client();
+                    $response  = $client->request('GET', $searchUrl);
+                    $content   = $response->getBody()->getContents();
+                    $json      = $this->jsonpDecode($content);
+
+                    if ($json->errCode == 0 && isset($json->uiData) && count($json->uiData) > 10) {
+//                        dd($json);
+                        foreach ($json->uiData as $data) {
+//                            dd($data);
+                            $videoId      = $data->data[0]->id; // 视频id
+                            $title        = $data->data[0]->title;
+                            $play_count   = $data->data[0]->playCount;
+                            $thumb        = $data->data[0]->posterPic;
+                            $original_url = $data->data[0]->webPlayUrl;
+                            // 去重
+                            if (!$video = Video::where('original_url', $original_url)->first()) {
+                                $video = new Video();
+                            }
+//                            dd($thumb, $play_count, $title, $videoId);
+                            if ($thumb && $title && $videoId) {
+                                // 第二步：拿到视频id，获取视频信息
+                                $infoUrl      = "https://h5vv.video.qq.com/getinfo?callback=txplayerJsonpCallBack_getinfo_934380&&charge=0&defaultfmt=auto&otype=json&guid=a96d6368975b40fd9fc8a7eb5d3a45e4&flowid=e8af7e3800936921036d09289f74f5ae_11001&platform=11001&sdtfrom=v3010&defnpayver=0&appVer=3.4.40&host=m.v.qq.com&ehost=https%3A%2F%2Fm.v.qq.com%2Fx%2Fchannel%2Fvideo%2Frecreation&refer=m.v.qq.com&sphttps=1&sphls=&_rnd=" . $timestamp . "&spwm=4&vid=" . $videoId . "&defn=auto&fhdswitch=&show1080p=false&dtype=1&clip=4&defnsrc=&fmt=auto&defsrc=1&_qv_rmt=C103p%2BIKA16110pdw%3D&_qv_rmt2=jw5bfArY152401F2g%3D&_1562141001953=";
+                                $infoResponse = $client->request('GET', $infoUrl);
+                                $infoContent  = $infoResponse->getBody()->getContents();
+                                $infoJson     = $this->jsonpDecode($infoContent, false);
+//                                dd($infoJson->vl);
+                                // 第三步：拿到视频地址，写入数据库
+                                if (isset($infoJson) && isset($infoJson->vl) && isset($infoJson->vl->vi) && count($infoJson->vl->vi) > 0) {
+                                    $videoInfo      = $infoJson->vl->vi[0];
+                                    $videoExtension = $videoInfo->fn;
+                                    $videoKey       = $videoInfo->fvkey;
+//                                    dd($videoInfo->ul->ui[0]->url);
+                                    $baseUrl        = $videoInfo->ul->ui[0]->url;
+                                    $time           = Carbon::now()->toDateTimeString();
+
+                                    if ($videoExtension && $videoKey) {
+                                        $url = $baseUrl.$videoExtension . '?vkey=' . $videoKey;
+//                                        dd($url);
+                                        $video->url          = $url;
+                                        $video->title        = $title;
+                                        $video->play_count   = $play_count;
+                                        $video->thumb        = $thumb;
+                                        $video->date         = $date;
+                                        $video->category_id  = $category->id;
+                                        $video->original_url = $original_url;
+                                        $video->play_time    = '';
+                                        $video->source_id    = 1; // 腾讯
+                                        $video->status       = 1;
+                                        $video->created_at   = $time;
+                                        $video->updated_at   = $time;
+                                        $video->save();
+                                    } else {
+                                        myLog('qq_video', ['data' => '【' . $category->id . $category->name . '】找不到url地址']);
+
+                                        sleep(1);
+
+                                        continue;
+                                    }
+                                }
+                            } else {
+                                myLog('qq_video', ['data' => '【' . $category->id . $category->name . '】找不到视频id']);
+
+                                sleep(1);
+
+                                continue;
+                            }
+
+                            sleep(2);
+                        }
+                    } else {
+                        myLog('qq_video', ['data' => '【' . $category->id . $category->name . '】获取列表失败']);
+
+                        sleep(1);
+
+                        continue;
+                    }
+                } catch (\Exception $e) {
+                    myLog('qq_video', ['data' => $category->name . '【' . $e->getLine() . '】' . $e->getMessage()]);
+
+                    sleep(1);
+
+                    continue;
+                }
+            }
+        }
+    }
+
+    /**
+     * @param $jsonp
+     * @param bool $assoc
+     * @return mixed
+     */
+    public function jsonpDecode($jsonp, $assoc = false)
+    {
+        $jsonp = trim($jsonp);
+        if (isset($jsonp[0]) && $jsonp[0] !== '[' && $jsonp[0] !== '{') {
+            $begin = strpos($jsonp, '(');
+            if (false !== $begin) {
+                $end = strrpos($jsonp, ')');
+                if (false !== $end) {
+                    $jsonp = substr($jsonp, $begin + 1, $end - $begin - 1);
+                }
+            }
+        }
+        return json_decode($jsonp, $assoc);
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,22 +15,23 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         //
-        'App\Console\Commands\FunSpider', // 搞笑
-        'App\Console\Commands\EntSpider', // 娱乐
-        'App\Console\Commands\ComicSpider', // 娱乐
-        'App\Console\Commands\WzrySpider', // 王者荣耀
-        'App\Console\Commands\DwrgSpider', // 第五人格
-        'App\Console\Commands\JdqsSpider', // 绝地求生
-        'App\Console\Commands\LolSpider', // 英雄联盟
-        'App\Console\Commands\HpjySpider', // 和平精英
-        'App\Console\Commands\FunFinalSpider', // 搞笑
-        'App\Console\Commands\EntFinalSpider', // 娱乐
-        'App\Console\Commands\ComicFinalSpider', // 娱乐
-        'App\Console\Commands\WzryFinalSpider', // 王者荣耀
-        'App\Console\Commands\DwrgFinalSpider', // 第五人格
-        'App\Console\Commands\JdqsFinalSpider', // 绝地求生
-        'App\Console\Commands\LolFinalSpider', // 英雄联盟
-        'App\Console\Commands\HpjyFinalSpider', // 和平精英
+//        'App\Console\Commands\FunSpider', // 搞笑
+//        'App\Console\Commands\EntSpider', // 娱乐
+//        'App\Console\Commands\ComicSpider', // 娱乐
+//        'App\Console\Commands\WzrySpider', // 王者荣耀
+//        'App\Console\Commands\DwrgSpider', // 第五人格
+//        'App\Console\Commands\JdqsSpider', // 绝地求生
+//        'App\Console\Commands\LolSpider', // 英雄联盟
+//        'App\Console\Commands\HpjySpider', // 和平精英
+//        'App\Console\Commands\FunFinalSpider', // 搞笑
+//        'App\Console\Commands\EntFinalSpider', // 娱乐
+//        'App\Console\Commands\ComicFinalSpider', // 娱乐
+//        'App\Console\Commands\WzryFinalSpider', // 王者荣耀
+//        'App\Console\Commands\DwrgFinalSpider', // 第五人格
+//        'App\Console\Commands\JdqsFinalSpider', // 绝地求生
+//        'App\Console\Commands\LolFinalSpider', // 英雄联盟
+//        'App\Console\Commands\HpjyFinalSpider', // 和平精英
+        'App\Console\Commands\QQVideoSpider', // 腾讯视频爬虫
 //        'App\Console\Commands\FunUrlSpider',
 //        'App\Console\Commands\EntUrlSpider',
 //        'App\Console\Commands\GameSpider', // 游戏
@@ -49,22 +50,23 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
-        $schedule->command('fun:spider')->everyTenMinutes();
-        $schedule->command('fun_final:spider')->everyTenMinutes();
-        $schedule->command('ent:spider')->everyTenMinutes();
-        $schedule->command('ent_final:spider')->everyTenMinutes();
-        $schedule->command('comic:spider')->everyTenMinutes();
-        $schedule->command('comic_final:spider')->everyTenMinutes();
-        $schedule->command('jdqs:spider')->everyTenMinutes();
-        $schedule->command('jdqs_final:spider')->everyTenMinutes();
-        $schedule->command('wzry:spider')->everyTenMinutes();
-        $schedule->command('wzry_final:spider')->everyTenMinutes();
-        $schedule->command('lol:spider')->everyTenMinutes();
-        $schedule->command('lol_final:spider')->everyTenMinutes();
-        $schedule->command('dwrg:spider')->everyTenMinutes();
-        $schedule->command('dwrg_final:spider')->everyTenMinutes();
-        $schedule->command('hpjy:spider')->everyTenMinutes();
-        $schedule->command('hpjy_final:spider')->everyTenMinutes();
+//        $schedule->command('fun:spider')->everyTenMinutes();
+//        $schedule->command('fun_final:spider')->everyTenMinutes();
+//        $schedule->command('ent:spider')->everyTenMinutes();
+//        $schedule->command('ent_final:spider')->everyTenMinutes();
+//        $schedule->command('comic:spider')->everyTenMinutes();
+//        $schedule->command('comic_final:spider')->everyTenMinutes();
+//        $schedule->command('jdqs:spider')->everyTenMinutes();
+//        $schedule->command('jdqs_final:spider')->everyTenMinutes();
+//        $schedule->command('wzry:spider')->everyTenMinutes();
+//        $schedule->command('wzry_final:spider')->everyTenMinutes();
+//        $schedule->command('lol:spider')->everyTenMinutes();
+//        $schedule->command('lol_final:spider')->everyTenMinutes();
+//        $schedule->command('dwrg:spider')->everyTenMinutes();
+//        $schedule->command('dwrg_final:spider')->everyTenMinutes();
+//        $schedule->command('hpjy:spider')->everyTenMinutes();
+//        $schedule->command('hpjy_final:spider')->everyTenMinutes();
+        $schedule->command('qq:video')->everyThirtyMinutes();
     }
 
     /**
