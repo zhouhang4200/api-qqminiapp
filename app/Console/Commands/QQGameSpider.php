e4eebb9fea4f81791d383c880d720f61e6b66a3f
zhouhang <zhouhang4200@163.com>
2019-07-10 22:01:24 +0800

    修改爬虫

--- app/Console/Commands/QQGameSpider.php
+++ app/Console/Commands/QQGameSpider.php
@@ -51,71 +51,74 @@ class QQGameSpider extends Command
 
         foreach ($baseUrls as $category_id => $baseUrl) {
             for ($i =1; $i <= 30; $i++) {
-                // 第五人格
-                $response = Request::get($baseUrl)
-                    ->addHeader('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36')
-                    ->timeout(5)
-                    ->send();
+                try {
+                    // 第五人格
+                    $response = Request::get($baseUrl)
+                        ->addHeader('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36')
+                        ->timeout(5)
+                        ->send();
 
-                $html = $response->body;
+                    $html = $response->body;
 
-                $document  = new Document();
-                $doc       = $document->load($html);
-                $documents = $doc->find('.feeds_video'); // 数组
-
-                if ($document && count($documents) > 0) {
-                    foreach ($documents as $document) {
-                        try {
-                            echo 2;
-                            $timestamp = Carbon::now()->subHours(1)->timestamp;
-                            $thumb = $document->first('img')->getAttribute('dsrc');
-                            $video_id = $document->getAttribute('data-vid');
-                            $video = new Video();
-                            $infoUrl      = "https://h5vv.video.qq.com/getinfo?callback=txplayerJsonpCallBack_getinfo_934380&&charge=0&defaultfmt=auto&otype=json&guid=a96d6368975b40fd9fc8a7eb5d3a45e4&flowid=e8af7e3800936921036d09289f74f5ae_11001&platform=11001&sdtfrom=v3010&defnpayver=0&appVer=3.4.40&host=m.v.qq.com&ehost=https%3A%2F%2Fm.v.qq.com%2Fx%2Fchannel%2Fvideo%2Frecreation&refer=m.v.qq.com&sphttps=1&sphls=&_rnd=" . $timestamp . "&spwm=4&vid=" . $video_id . "&defn=auto&fhdswitch=&show1080p=false&dtype=1&clip=4&defnsrc=&fmt=auto&defsrc=1&_qv_rmt=C103p%2BIKA16110pdw%3D&_qv_rmt2=jw5bfArY152401F2g%3D&_1562141001953=";
-                            $client    = new Client();
-                            $infoResponse = $client->request('GET', $infoUrl);
-                            $infoContent  = $infoResponse->getBody()->getContents();
-                            $infoJson     = $this->jsonpDecode($infoContent, false);
+                    $document  = new Document();
+                    $doc       = $document->load($html);
+                    $documents = $doc->find('.feeds_video'); // 数组
+                    echo $i;
+                    if ($document && count($documents) > 0) {
+                        foreach ($documents as $document) {
+                            try {
+                                $timestamp = Carbon::now()->subHours(1)->timestamp;
+                                $thumb = $document->first('img')->getAttribute('dsrc');
+                                $video_id = $document->getAttribute('data-vid');
+                                $video = new Video();
+                                $infoUrl      = "https://h5vv.video.qq.com/getinfo?callback=txplayerJsonpCallBack_getinfo_934380&&charge=0&defaultfmt=auto&otype=json&guid=a96d6368975b40fd9fc8a7eb5d3a45e4&flowid=e8af7e3800936921036d09289f74f5ae_11001&platform=11001&sdtfrom=v3010&defnpayver=0&appVer=3.4.40&host=m.v.qq.com&ehost=https%3A%2F%2Fm.v.qq.com%2Fx%2Fchannel%2Fvideo%2Frecreation&refer=m.v.qq.com&sphttps=1&sphls=&_rnd=" . $timestamp . "&spwm=4&vid=" . $video_id . "&defn=auto&fhdswitch=&show1080p=false&dtype=1&clip=4&defnsrc=&fmt=auto&defsrc=1&_qv_rmt=C103p%2BIKA16110pdw%3D&_qv_rmt2=jw5bfArY152401F2g%3D&_1562141001953=";
+                                $client    = new Client();
+                                $infoResponse = $client->request('GET', $infoUrl);
+                                $infoContent  = $infoResponse->getBody()->getContents();
+                                $infoJson     = $this->jsonpDecode($infoContent, false);
 //                                dd($infoJson->vl);
-                            // 第三步：拿到视频地址，写入数据库
-                            if (isset($infoJson) && isset($infoJson->vl) && isset($infoJson->vl->vi) && count($infoJson->vl->vi) > 0) {
-                                $videoInfo      = $infoJson->vl->vi[0];
-                                $videoExtension = $videoInfo->fn;
-                                $videoKey       = $videoInfo->fvkey;
+                                // 第三步：拿到视频地址，写入数据库
+                                if (isset($infoJson) && isset($infoJson->vl) && isset($infoJson->vl->vi) && count($infoJson->vl->vi) > 0) {
+                                    $videoInfo      = $infoJson->vl->vi[0];
+                                    $videoExtension = $videoInfo->fn;
+                                    $videoKey       = $videoInfo->fvkey;
 //                                    dd($infoJson);
-                                $baseUrl = $videoInfo->ul->ui[0]->url;
-                                $time    = Carbon::now()->toDateTimeString();
-                                $title = $videoInfo->ti;
-                                $play_count = '0';
-                                $original_url = '';
-                                $play_time = $infoJson->preview;
+                                    $baseUrl = $videoInfo->ul->ui[0]->url;
+                                    $time    = Carbon::now()->toDateTimeString();
+                                    $title = $videoInfo->ti;
+                                    $play_count = '0';
+                                    $original_url = '';
+                                    $play_time = $infoJson->preview;
 
-                                if ($videoExtension && $videoKey) {
-                                    $url = $baseUrl . $videoExtension . '?vkey=' . $videoKey;
+                                    if ($videoExtension && $videoKey) {
+                                        $url = $baseUrl . $videoExtension . '?vkey=' . $videoKey;
 //                                        dd($url);
-                                    $video->url          = $url;
-                                    $video->title        = $title;
-                                    $video->play_count   = $play_count;
-                                    $video->thumb        = $thumb;
-                                    $video->date         = $date;
-                                    $video->category_id  = $category_id;
-                                    $video->original_url = $original_url;
-                                    $video->play_time    = $play_time;
-                                    $video->source_id    = 1; // 腾讯
-                                    $video->status       = 1;
-                                    $video->created_at   = $time;
-                                    $video->updated_at   = $time;
-                                    $video->save();
-                                } else {
-                                    myLog('qq_video', ['data' => '【' . $category_id . '】找不到url地址']);
-                                    sleep(1);
+                                        $video->url          = $url;
+                                        $video->title        = $title;
+                                        $video->play_count   = $play_count;
+                                        $video->thumb        = $thumb;
+                                        $video->date         = $date;
+                                        $video->category_id  = $category_id;
+                                        $video->original_url = $original_url;
+                                        $video->play_time    = $play_time;
+                                        $video->source_id    = 1; // 腾讯
+                                        $video->status       = 1;
+                                        $video->created_at   = $time;
+                                        $video->updated_at   = $time;
+                                        $video->save();
+                                    } else {
+                                        myLog('qq_video', ['data' => '【' . $category_id . '】找不到播放地址']);
+                                        sleep(1);
+                                    }
                                 }
+                            } catch (\Exception $e) {
+                                myLog('qq_game_error', ['date' => $e->getLine().'找不到列表地址标签'.$e->getMessage()]);
                             }
-                        } catch (\Exception $e) {
-                            myLog('qq_game_error', ['date' => $e->getLine().'找不到原始地址标签'.$e->getMessage()]);
-                            continue;
                         }
                     }
+                    sleep(3);
+                } catch (\Exception $e) {
+                    myLog('qq_game_error', ['date' => $e->getLine().'列表页错误'.$e->getMessage()]);
                 }
             }
         }
