eb09610b9dd8fb3a973bc491f47e7f670dbf640e
zhouhang <zhouhang4200@163.com>
2019-07-11 15:37:29 +0800

    增加注释说明，删除多余代码

--- app/Console/Commands/QQGameSpider.php
+++ app/Console/Commands/QQGameSpider.php
@@ -58,42 +58,38 @@ class QQGameSpider extends Command
         }
 
         if ($category_id == 1) { // 游戏
-            $url = 'https://m.v.qq.com/x/channel/video/game?pagelet=1&pageContext='.$pageContext.'&refreshContext='.$refreshContext.'&request=hot_videoline';
+            $url = 'https://m.v.qq.com/x/channel/video/game?pagelet=1&pageContext=' . $pageContext . '&refreshContext=' . $refreshContext . '&request=hot_videoline';
         } elseif ($category_id == 2) { // 娱乐
-            $url = 'https://m.v.qq.com/x/channel/video/recreation?pagelet=1&pageContext='.$pageContext.'&refreshContext='.$refreshContext.'&request=hot_videoline';
+            $url = 'https://m.v.qq.com/x/channel/video/recreation?pagelet=1&pageContext=' . $pageContext . '&refreshContext=' . $refreshContext . '&request=hot_videoline';
         } else {
             return false;
         }
 
-        $client    = new Client();
+        // 获取列表页面
+        $client       = new Client();
         $infoResponse = $client->request('GET', $url);
         $infoContent  = $infoResponse->getBody()->getContents();
-//        dd($infoContent);
 
         // 视频图片和视频id
         preg_match_all('~class=\\\"poster\\\" dsrc=\\\"(.*?)\\\"~', $infoContent, $thumbs);
         preg_match_all('~data-vid=\\\"(.*?)\\\"\>~', $infoContent, $video_ids);
 
-//        dd($thumbs, $video_ids, $matches);
         if ($thumbs && count($thumbs) > 0) {
-            foreach($thumbs[1] as $k => $thumb) {
+            foreach ($thumbs[1] as $k => $thumb) {
                 $timestamp = Carbon::now()->timestamp;
-                $video_id = $video_ids[1][$k];
-//                dd($video_id);
+                $video_id  = $video_ids[1][$k];
                 try {
-                    $video = new Video();
-                    $infoUrl      = "https://h5vv.video.qq.com/getinfo?callback=txplayerJsonpCallBack_getinfo_934380&&charge=0&defaultfmt=auto&otype=json&guid=a96d6368975b40fd9fc8a7eb5d3a45e4&flowid=e8af7e3800936921036d09289f74f5ae_11001&platform=11001&sdtfrom=v3010&defnpayver=0&appVer=3.4.40&host=m.v.qq.com&ehost=https%3A%2F%2Fm.v.qq.com%2Fx%2Fchannel%2Fvideo%2Frecreation&refer=m.v.qq.com&sphttps=1&sphls=&_rnd=" . $timestamp . "&spwm=4&vid=" . $video_id . "&defn=auto&fhdswitch=&show1080p=false&dtype=1&clip=4&defnsrc=&fmt=auto&defsrc=1&_qv_rmt=C103p%2BIKA16110pdw%3D&_qv_rmt2=jw5bfArY152401F2g%3D&_1562141001953=";
-                    $client    = new Client();
-                    $infoResponse = $client->request('GET', $infoUrl);
-                    $detailContent  = $infoResponse->getBody()->getContents();
-                    $infoJson     = $this->jsonpDecode($detailContent, false);
-//                                dd($infoJson->vl);
+                    $video         = new Video();
+                    $infoUrl       = "https://h5vv.video.qq.com/getinfo?callback=txplayerJsonpCallBack_getinfo_934380&&charge=0&defaultfmt=auto&otype=json&guid=a96d6368975b40fd9fc8a7eb5d3a45e4&flowid=e8af7e3800936921036d09289f74f5ae_11001&platform=11001&sdtfrom=v3010&defnpayver=0&appVer=3.4.40&host=m.v.qq.com&ehost=https%3A%2F%2Fm.v.qq.com%2Fx%2Fchannel%2Fvideo%2Frecreation&refer=m.v.qq.com&sphttps=1&sphls=&_rnd=" . $timestamp . "&spwm=4&vid=" . $video_id . "&defn=auto&fhdswitch=&show1080p=false&dtype=1&clip=4&defnsrc=&fmt=auto&defsrc=1&_qv_rmt=C103p%2BIKA16110pdw%3D&_qv_rmt2=jw5bfArY152401F2g%3D&_1562141001953=";
+                    $client        = new Client();
+                    $infoResponse  = $client->request('GET', $infoUrl);
+                    $detailContent = $infoResponse->getBody()->getContents();
+                    $infoJson      = $this->jsonpDecode($detailContent, false);
                     // 第三步：拿到视频地址，写入数据库
                     if (isset($infoJson) && isset($infoJson->vl) && isset($infoJson->vl->vi) && count($infoJson->vl->vi) > 0) {
                         $videoInfo      = $infoJson->vl->vi[0];
                         $videoExtension = $videoInfo->fn;
                         $videoKey       = $videoInfo->fvkey;
-    //                                    dd($infoJson);
                         $baseUrl      = $videoInfo->ul->ui[0]->url;
                         $time         = Carbon::now()->toDateTimeString();
                         $title        = $videoInfo->ti;
@@ -103,7 +99,6 @@ class QQGameSpider extends Command
 
                         if ($videoExtension && $videoKey) {
                             $url = $baseUrl . $videoExtension . '?vkey=' . $videoKey;
-    //                                        dd($url);
                             $video->url          = $url;
                             $video->title        = $title;
                             $video->play_count   = $play_count;
@@ -128,18 +123,19 @@ class QQGameSpider extends Command
             }
         }
 
+        // 两个类目的列表页的正则不一样
         if ($category_id == 2) {
 //            preg_match_all('~\<div class=\"hide\"\>responsedata=\/\*\{&quot\;channelTag&quot\;\:\&quot\;recreation\&quot\;\,\&quot\;pageContext\&quot\;\:&quot\;(.*?)&quot;,&quot;refreshContext&quot;:&quot;(.*?)&quot\;\,\&quot\;hasNextPage&quot\;\:true\}\*\/\<\/div\>~', $infoContent, $matches);
             preg_match_all('~\<div class=\\\"hide\\\"\>responsedata=\/\*\{&quot\;channelTag&quot\;\:\&quot\;recreation\&quot\;\,\&quot\;pageContext\&quot\;\:&quot\;(.*?)&quot;,&quot;refreshContext&quot;:&quot;(.*?)&quot\;\,\&quot\;hasNextPage&quot\;\:true\}\*\/\<\/div\>~', $infoContent, $matches);
         } elseif ($category_id == 1) {
 //                    preg_match_all('~\<div class=\"hide\"\>responsedata=\/\*\{\&quot\;channelTag&quot\;\:\&quot\;recreation\&quot\;\,\&quot\;pageContext\&quot\;\:&quot\;(.*?)\&quot\;\,\&quot\;refreshContext\&quot\;\:\&quot\;(.*?)&quot\;\,&quot\;hasNextPage&quot\;\:true\}\*\/\<\/div\>~', $html, $matches);
             preg_match_all('~\<div class=\"hide\"\>responsedata=\/\*\{&quot;channelTag&quot;:&quot;game&quot;,&quot;pageContext&quot;:&quot;(.*?)&quot;,&quot;refreshContext&quot;:&quot;(.*?)&quot;,&quot;hasNextPage&quot;:true\}\*\/\<\/div\>~', $infoContent, $matches);
+        } else {
+            $matches = [];
         }
 
-//        preg_match_all('~\<div class=\\\"hide\\\"\>responsedata=\/\*\{&quot\;channelTag&quot\;\:\&quot\;recreation\&quot\;\,\&quot\;pageContext\&quot\;\:&quot\;(.*?)&quot;,&quot;refreshContext&quot;:&quot;(.*?)&quot\;\,\&quot\;hasNextPage&quot\;\:true\}\*\/\<\/div\>~', $infoContent, $matches);
-//        dd($matches);
         if ($matches && count($matches) > 0 && count($matches[1]) > 0) {
-            $pageContext = $matches[1][0];
+            $pageContext    = $matches[1][0];
             $refreshContext = $matches[2][0];
             $this->getList($category_id, $pageContext, $refreshContext, $date);
         }
@@ -152,15 +148,13 @@ class QQGameSpider extends Command
      */
     public function handle()
     {
-        $date = Carbon::now()->toDateString();
+        $date     = Carbon::now()->toDateString();
         $baseUrls = [
-            '1' => 'https://m.v.qq.com/x/channel/video/game',
-            '2' => 'https://m.v.qq.com/x/channel/video/recreation',
+            '1' => 'https://m.v.qq.com/x/channel/video/game',  // 游戏
+            '2' => 'https://m.v.qq.com/x/channel/video/recreation', // 娱乐
         ];
 
         foreach ($baseUrls as $category_id => $baseUrl) {
-            $timestamp = Carbon::now()->timestamp;
-//            echo $category_id;
             try {
                 // 第一次爬原始页面，找数据
 //                $puppeteer = new Puppeteer([
@@ -184,7 +178,7 @@ class QQGameSpider extends Command
 //
 //                $page->goto($baseUrl, ['timeout' => 60000]);
 //                $html = $page->content(); // Prints the HTML
-
+                // 游戏地址可能会出错
                 $response = Request::get($baseUrl)
                     ->addHeader('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36')
                     ->timeout(5)
@@ -192,57 +186,33 @@ class QQGameSpider extends Command
 
                 $html = $response->body;
 
-                if ($category_id == 2) {
+                if ($category_id == 2) { // 娱乐
                     preg_match_all('~\<div class=\"hide\"\>responsedata=\/\*\{&quot\;channelTag&quot\;\:\&quot\;recreation\&quot\;\,\&quot\;pageContext\&quot\;\:&quot\;(.*?)&quot;,&quot;refreshContext&quot;:&quot;(.*?)&quot\;\,\&quot\;hasNextPage&quot\;\:true\}\*\/\<\/div\>~', $html, $matches);
-                } elseif ($category_id == 1) {
+                } elseif ($category_id == 1) { // 游戏
                     preg_match_all('~\<div class=\"hide\"\>responsedata=\/\*\{&quot;channelTag&quot;:&quot;game&quot;,&quot;pageContext&quot;:&quot;(.*?)&quot;,&quot;refreshContext&quot;:&quot;(.*?)&quot;,&quot;hasNextPage&quot;:true\}\*\/\<\/div\>~', $html, $matches);
                 } else {
                     continue;
                 }
-//                dd($matches);
+
                 if ($matches && count($matches) > 0 && count($matches[1]) > 0) {
-                    $pageContext = $matches[1][0];
+                    $pageContext    = $matches[1][0];
                     $refreshContext = $matches[2][0];
                     // 如果其中一个分类条数大于200，则返回false，这边跳出循环，循环下一个分类
                     $result = $this->getList($category_id, $pageContext, $refreshContext, $date);
 
-//                    if (!$result) {
-//                        continue;
-//                    }
+                    if (!$result) {
+                        continue;
+                    }
                 }
             } catch (\Exception $e) {
-                myLog('qq_game_error', ['data' => $e->getLine().$e->getMessage()]);
-//                dd($e->getLine().'行:'.$e->getMessage());
+                myLog('qq_game_error', ['data' => $e->getLine() .'行：'. $e->getMessage()]);
                 continue;
             }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //            for ($i =1; $i <= 30; $i++) {
 //                try {
+//            $timestamp = Carbon::now()->timestamp;
 //                    // 第五人格
 //                    $response = Request::get($baseUrl)
 //                        ->addHeader('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36')
